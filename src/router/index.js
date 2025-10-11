060dc19859e541f84ce8d6bd42a1bc698440218e
Nicholas Sica <nick.sica16@gmail.com>
2025-10-11 12:25:27 +0000

    TEST ( FORM ) : Testando localStorage

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+    }
+  ],
 })
 
 export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <p>your name: {{ name }}</p>
+    <input type="text" v-model="name" placeholder="Whats ur name?" />
+    <Button type="submit" @click="submit">submit</Button>
+    <br>
+    <button @click="callName">Whats MY name?</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const name = ref("");
+const submit = () => {
+  alert(`Hello ${name.value}, welcome to Vue 3!`);
+  localStorage.setItem("name", name.value);
+  name.value = "";
+};
+const callName = () => {
+  name.value = "";
+  const storedName = localStorage.getItem("name");
+  if (storedName) {
+    alert(`Your name is ${storedName}`);
+    name.value = storedName;
+  } else {
+    alert("No name found in local storage.");
+  }
+};
+
+</script>
