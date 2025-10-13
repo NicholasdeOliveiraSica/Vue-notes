3ffe6fadcb144140b63de82ba3a2fb904f15380f
Nicholas Sica <nick.sica16@gmail.com>
2025-10-13 00:18:41 +0000

    chore(DX): Resposta visual ao rodar o projeto

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    {
+      name: 'foguetinho-log',
+      configureServer() {
+        console.log('\n🚀 Preview rodando em \x1b[1m\x1b[36mlocalhost:5173\x1b[0m\n')
+      },
+    },
   ],
   resolve: {
     alias: {
